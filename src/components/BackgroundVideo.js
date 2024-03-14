63394c32601cdb9d6863ee533993ba011bd0dcc8
Steve Evrard <stevelevrard@gmail.com>
2024-03-14 15:19:13 -0400

    updatevideo background

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -5,7 +5,7 @@ import truckingVideo from '../assets/trucking.mp4';
 const BackgroundVideo = () => {
   return (
     <div className="video-background">
-      <video autoPlay loop muted style={{ width: '100%', height: '100%', objectFit: 'cover' }}>
+      <video autoPlay loop muted playsInline style={{ width: '100%', height: '100%', objectFit: 'cover', pointerEvents: 'none' }}>
         <source src={truckingVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
